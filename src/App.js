667bef9148de87430c3ff627e77e2b1a390b3bb1
Alec Barber <alecbarber23@gmail.com>
2023-03-08 18:36:57 -0500

    created form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Form from "./Components/Form";
 
 const App = () => {
   return (
     <>
       <h1>Bloomtech Eats</h1>
       <p>oh geez, here we go!</p>
+      <Form />
     </>
   );
 };
