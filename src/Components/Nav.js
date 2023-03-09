14066f21c97380ea13a9166b16e075b23363f7e0
Alec Barber <alecbarber23@gmail.com>
2023-03-09 09:30:37 -0500

    basic nav styling

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+
+const NavStyle = styled.nav`
+    background: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 150px;
+    border: solid 1px black;
+    border-radius: 5px;
+
+    a {
+        font-size: 1.6rem;
+        color: crimson;
+        text-decoration: none;
+    }
+
+    a:hover {
+        font-size: 1.6rem;
+        color: gray;
+    }
+`
 
 export default function Nav (props) {
 
 
     return (
-        <nav>
-            <a href="/">Home</a>
-            <a href="">Help</a>
-        </nav>
+        <NavStyle>
+            <nav>
+                <a href="/">Home</a>&nbsp;
+                <a href="">Help</a>
+            </nav>
+        </NavStyle>
     )
 }
